8442b598c91e427601f25b2a68cb32fd5e8ee2a1
Bernard Bacani <bernard.bacani@gmail.com>
2022-11-13 16:49:54 +0100

    Enable CORS

--- src/main/java/hr/fer/dsd/privtap/config/WebSecurityConfig.java
+++ src/main/java/hr/fer/dsd/privtap/config/WebSecurityConfig.java
@@ -2,9 +2,18 @@ package hr.fer.dsd.privtap.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
@@ -15,14 +24,32 @@ public class WebSecurityConfig {
 
         http
                 .oauth2Login()
+                .successHandler(successHandler())
                 .defaultSuccessUrl("/loginSuccess")
                 .failureUrl("/loginFailure")
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                .mvcMatchers("/").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
         return http.build();
     }
 
+    private AuthenticationSuccessHandler successHandler() {
+        return new SimpleUrlAuthenticationSuccessHandler() {
+            public void onAuthenticationSuccess(HttpServletRequest request,
+                                                HttpServletResponse response, Authentication authentication)
+                    throws IOException {
+                HttpSession session = request.getSession(true);
+                session.setMaxInactiveInterval(60 * 180);
+                response.getWriter().println("LoginSuccessful");
+                response.addHeader("Access-Control-Allow-Origin",
+                        "http://localhost:3000, http://privtap-bucket.s3-website.eu-central-1.amazonaws.com/");
+                response.addHeader("Access-Control-Allow-Credentials", "true");
+            }
+        };
+    }
 
 }
-
-
