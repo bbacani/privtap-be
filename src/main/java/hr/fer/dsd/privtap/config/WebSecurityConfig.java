027c8b55ffb22ddf82c1409799d88ad2624f4f53
giuliadauria <giulia.dauria@mail.polimi.it>
2022-12-12 14:40:19 +0100

    small changes in webconfig v2

--- src/main/java/hr/fer/dsd/privtap/config/WebSecurityConfig.java
+++ src/main/java/hr/fer/dsd/privtap/config/WebSecurityConfig.java
@@ -91,45 +91,46 @@ public class WebSecurityConfig {
         return http.build();
     }
 
-        @Bean
-        @Order(2)
-        public SecurityFilterChain filterChainServiceProvider(HttpSecurity http) throws Exception {
-            http
-                    .cors()
-                    .and()
-                    .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    .and()
-                    .csrf()
-                    .disable()
-                    .formLogin()
-                    .disable()
-                    .httpBasic()
-                    .disable()
-                    .exceptionHandling()
-                    .authenticationEntryPoint(new RestAuthenticationEntryPoint())
-                    .and()
-                    .authorizeRequests()
-                    .antMatchers("/**").permitAll()
-                    .antMatchers("/serviceprovider/auth/**", "/serviceprovider/oauth2/**").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
-                    .oauth2Login()
-                    .authorizationEndpoint()
-                    .baseUri("/oauth2/authorize")
-                    .authorizationRequestRepository(cookieAuthorizationRequestRepository())
-                    .and()
-                    .redirectionEndpoint()
-                    .baseUri("/oauth2/callback/*")
-                    .and()
-                    .userInfoEndpoint()
-                    .userService(customOAuth2ServiceProviderService)
-                    .and()
-                    .successHandler(oAuth2AuthenticationSuccessHandler)
-                    .failureHandler(oAuth2AuthenticationFailureHandler);
+    @Bean
+    @Order(2)
+    public SecurityFilterChain filterChainServiceProvider(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf()
+                .disable()
+                .formLogin()
+                .disable()
+                .httpBasic()
+                .disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(new RestAuthenticationEntryPoint())
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .antMatchers("/serviceprovider/auth/**", "/serviceprovider/oauth2/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Login()
+                .authorizationEndpoint()
+                .baseUri("/oauth2/authorize")
+                .authorizationRequestRepository(cookieAuthorizationRequestRepository())
+                .and()
+                .redirectionEndpoint()
+                .baseUri("/oauth2/callback/*")
+                .and()
+                .userInfoEndpoint()
+                .userService(customOAuth2ServiceProviderService)
+                .and()
+                .successHandler(oAuth2AuthenticationSuccessHandler)
+                .failureHandler(oAuth2AuthenticationFailureHandler);
 
-            http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
-            return http.build();
-        }
+        return http.build();
+    }
 }
