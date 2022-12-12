3bac9228a3812b73edc0a9dd3ba083d06c930be7
federicatommasini <federica.tommasini@mail.polimi.it>
2022-12-12 10:12:03 +0100

    division in serviceProvider and Platform, fixing services and divide userservice

---
+++ src/main/java/hr/fer/dsd/privtap/domain/entities/ServiceProviderEntity.java
@@ -0,0 +1,21 @@
+package hr.fer.dsd.privtap.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "serviceProviders")
+public class ServiceProviderEntity {
+
+    @Id
+    private String id;
+    private String username;
+    private String email;
+    private String platformId;
+}
